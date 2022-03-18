e71142c5bf41c42df4d46fba287021e88980fb04
SeanReyboz <seanreyboz@protonmail.com>
2022-03-18 11:12:19 +0100

    Added all stats on /stats/

--- src/routes/statistics.js
+++ src/routes/statistics.js
@@ -8,6 +8,56 @@ const Stats = require("../controllers/stats");
 // Import des différents modèles à utiliser
 const SeanceModel = require("../models/Seance");
 
+// Pour se faciliter la vie, retourner toutes les différentes stats sur la même route
+router.get("/", async (req, res) => {
+  try {
+    // Récupérer les bpms, pressions et durée de chaque séance
+    const seances = await SeanceModel.aggregate([
+      { $sort: { date: -1 } },
+      { $project: { bpm: 1, pressions: 1, duration: 1 } },
+    ]);
+
+    // Données BPM
+    const { totalBpms, totalSeance } = Stats.calculateTotalBpms(seances);
+    const allTimeBpmAverage = Math.round(totalBpms / totalSeance);
+
+    const minBpm = Stats.getMinBpm(seances);
+    const maxBpm = Stats.getMaxBpm(seances);
+
+    // Données pressions
+    const { totalPressions } = Stats.calculateTotalPressions(seances);
+    const averagePressionsPerSession = totalPressions / totalSeance;
+
+    // Données durée
+    const { totalDuration } = Stats.calculateTotalDuration(seances);
+    const averageDurationPerSession = totalDuration / totalSeance;
+
+    res.status(200).json({
+      bpm: {
+        average: allTimeBpmAverage,
+        min: minBpm,
+        max: maxBpm,
+      },
+      pressions: {
+        total: totalPressions,
+        average: averagePressionsPerSession,
+      },
+      duration: {
+        total: totalDuration,
+        average: averageDurationPerSession,
+      },
+      // infos générales sur les séances
+      totalSeance,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "An error occured in the stats route trying to GET /",
+      error,
+    });
+  }
+});
+
 // Voir la moyenne all time de bpm
 router.get("/bpm", async (req, res) => {
   try {
